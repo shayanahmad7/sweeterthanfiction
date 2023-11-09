2cd517745a58ed11c2d476d6fda4aec0bfeef728
unknown <sa6097@nyu.edu>
2023-11-09 01:45:07 -0500

    set up basic web app and mongodb server for local host, will work on deployment now

--- app.mjs
+++ app.mjs
@@ -1,9 +1,26 @@
-import express from 'express'
-import path from 'path'
+import express from 'express';
+import mongoose from 'mongoose';
 import { fileURLToPath } from 'url';
+import path from 'path';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.listen(process.env.PORT || 3000);
+// MongoDB connection setup
+const mongoDB = 'mongodb://127.0.0.1:27017/sweeterthanfiction';
+
+mongoose.connect(mongoDB)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+// Simple root route
+app.get('/', (req, res) => {
+  res.send('Welcome to Sweeter Than Fiction!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
