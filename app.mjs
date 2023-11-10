1bcdafa7ebd06a07ec3187ade1bfd39a9d20dfa3
unknown <sa6097@nyu.edu>
2023-11-09 22:31:14 -0500

    Update app.mjs with environment variables and MongoDB connection

--- app.mjs
+++ app.mjs
@@ -1,25 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import './config.mjs'; // Ensure this is at the top to load environment variables first
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-// MongoDB connection setup
-const mongoDB = 'mongodb://127.0.0.1:27017/sweeterthanfiction';
-
-mongoose.connect(mongoDB)
+// MongoDB connection setup using the DB_URI from .env file loaded by config.mjs
+const dbURI = process.env.DB_URI;
+mongoose.connect(dbURI)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Simple root route
 app.get('/', (req, res) => {
   res.send('Welcome to Sweeter Than Fiction!');
 });
 
-// Start the server
+// Start the server using the PORT from .env file, default to 3000 if not specified
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
