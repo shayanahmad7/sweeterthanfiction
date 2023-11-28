b967bf14b691a8e36206da2e7254d88f43b112e2
unknown <sa6097@nyu.edu>
2023-11-28 03:43:37 -0500

    Implement user registration and login

--- app.mjs
+++ app.mjs
@@ -3,12 +3,11 @@ import mongoose from 'mongoose';
 import session from 'express-session';
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs'; 
 import path from 'path';
 import { engine } from 'express-handlebars';
-import flash from 'connect-flash'; // Flash messages middleware
-import './config.mjs'; // Load environment variables
-import User from './models/user.js'; // Import the User model
+import './config.mjs';
+import User from './models/user.js'; 
 
 const app = express();
 const __dirname = path.resolve();
@@ -22,101 +21,96 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files (like CSS, images, JavaScript)
+// Serve static files (like CSS)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/sweeterthanfiction', { useNewUrlParser: true, useUnifiedTopology: true })
+// MongoDB connection setup
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-// Configure Passport for user authentication
-passport.use(new LocalStrategy(async (username, password, done) => {
-  try {
-    const user = await User.findOne({ username });
-    if (!user || !await bcrypt.compare(password, user.password)) {
-      return done(null, false, { message: 'Incorrect username or password.' });
+// Passport local strategy configuration for authentication
+passport.use(new LocalStrategy(
+  async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user || !await bcrypt.compare(password, user.password)) {
+        return done(null, false, { message: 'Incorrect username or password.' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
     }
-    return done(null, user);
-  } catch (err) {
-    return done(err);
   }
-}));
+));
 
-// Serialize and deserialize user for session management
+// Serialize user
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
+// Deserialize user
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await User.findById(id);
+    const user = await User.findById(id); // Changed to use async/await
     done(null, user);
-  } catch (err) {
-    done(err);
+  } catch (error) {
+    done(error);
   }
 });
 
-// Express session setup
-app.use(session({ secret: 'some secret', resave: false, saveUninitialized: false }));
 
-// Initialize Passport and session management
+// Express and Passport session setup
+app.use(session({ secret: 'some secret', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Flash messages middleware setup
-app.use(flash());
-
-// Middleware to parse form data
+// Parsing form data
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 // Home route
 app.get('/', (req, res) => res.render('home', { title: 'Welcome to Sweeter Than Fiction!' }));
 
-// Registration page route
+// Route to display the Registration page
 app.get('/register', (req, res) => {
-  const messages = req.flash('error');
-  res.render('register', { messages });
+  res.render('register');
 });
 
-// Login page route
+// Route to display the Login page
 app.get('/login', (req, res) => {
-  const messages = req.flash('error');
-  res.render('login', { messages });
+  res.render('login');
 });
 
-// Process registration form submission
+// Registration route
 app.post('/register', async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const newUser = new User({
-      name: req.body.name,
-      username: req.body.username,
-      password: hashedPassword
-    });
-    await newUser.save();
+    const user = new User({ name: req.body.name, username: req.body.username, password: hashedPassword });
+    await user.save();
     res.redirect('/login');
   } catch (err) {
-    req.flash('error', 'Registration failed: ' + err.message);
     res.redirect('/register');
   }
 });
 
-// Process login form submission
+// Login route
 app.post('/login', passport.authenticate('local', {
+  successRedirect: '/userhome',
   failureRedirect: '/login',
-  failureFlash: true
-}), (req, res) => {
-  res.redirect('/userhome');
-});
+  failureFlash: false
+}));
 
-// User's home page route
+// User home route
 app.get('/userhome', (req, res) => {
   if (!req.user) return res.redirect('/login');
-  res.render('userhome', { title: 'Your Swiftie Home', name: req.user.name });
+  res.render('userhome', { name: req.user.name });
 });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
